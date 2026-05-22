915f05ccc8d01b9b0c9fe810bc51a5bfa46b7188
nethramathisu <nethra.gmr@gmail.com>
2026-05-23 01:41:57 +0530

    updated CORS Issues  backend

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://pet-adoption-we.netlify.app/login"
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+app.options("*", cors());
 
 app.use(express.json());
 
