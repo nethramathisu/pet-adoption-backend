491ae61d284ae9487966351f04fc9f00c7a2a335
nethramathisu <nethra.gmr@gmail.com>
2026-06-19 13:52:48 +0530

    Fixed server cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // ✅ Handle preflight requests for all routes
-app.options("/*", cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
 
